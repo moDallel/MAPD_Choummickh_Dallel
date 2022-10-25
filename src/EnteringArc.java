5e7f9000c2573c6cae55c12999eadf296977304c
moDall <mohamed_dallel@outlook.com>
2022-10-25 20:59:53 +0200

    first commit

---
+++ src/EnteringArc.java
@@ -0,0 +1,14 @@
+
+public class EnteringArc extends Arc {
+	public Place getPlace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
---
+++ src/IPetriNetwork.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+
+public interface IPetriNetwork {
+	public LinkedList<Transition> firableTransitions();
+	public boolean fire(Transition t);
+	
+	/**
+	 * Adds the place.
+	 *
+	 * @param nbTokens number of tokens >= 0 
+	 * @return null
+	 */
+	void addPlace(int nbTokens);
+	void addTransition();
+	void addEnteringArc(int weight, Place p, Transition t);
+	void addExitingArc(int weight, Place p, Transition t);
+	void addZeroArc(Place p, Transition t);
+	void addEmptyingArc(Place p, Transition t);
+	boolean removeArc(Arc a);
+	boolean removeTransition(Transition t);
+	boolean removePlace(Place p);
+	void setTokensNumber(Place p, int nbTokens);
+	void setPoidsArc(Arc a, int weight);
+
+}
